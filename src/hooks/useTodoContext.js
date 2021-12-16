b4c38de1a0978336682514241db39153e371d23a
Naem Bhuiyan <32921299+NaemBhuiyan@users.noreply.github.com>
2021-12-16 18:06:12 +0600

    Update useTodoContext.js

--- src/hooks/useTodoContext.js
+++ src/hooks/useTodoContext.js
@@ -5,7 +5,7 @@ const useTodoContext = () => {
   const context = useContext(AppContext);
   if (!context) {
     throw new Error(
-      "Auth context provider not found, make sure you're using auth components correctly."
+      "Todo context provider not found, make sure you're using auth components correctly."
     );
   }
   return context;
